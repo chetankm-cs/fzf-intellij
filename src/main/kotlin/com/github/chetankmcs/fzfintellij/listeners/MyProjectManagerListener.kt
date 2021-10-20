65ce13fb4766889e20dc6803b0556fc6b6f50e92
GitHub Action <action@github.com>
2021-10-20 17:50:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/chetankmcs/fzfintellij/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/chetankmcs/fzfintellij/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.chetankmcs.fzfintellij.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.chetankmcs.fzfintellij.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/chetankmcs/fzfintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chetankmcs/fzfintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chetankmcs.fzfintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chetankmcs.fzfintellij.MyBundle
 
 class MyApplicationService {
 
